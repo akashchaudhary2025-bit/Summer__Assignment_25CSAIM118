95eb5bf9bfea9c8aa0a0d7d822b49770684e614b
akashchaudhary2025-bit <akash.chaudhary2025@glbajajgroup.org>
2026-06-15 17:22:29 +0530

    Delete Day 8 directory

--- Day 8/Character triangle.py
+++
@@ -1,8 +0,0 @@
-# Character Triangle Pattern
-
-rows = int(input("Enter the number of rows: "))
-
-for i in range(1, rows + 1):
-    for j in range(65, 65 + i):   # ASCII value of 'A' is 65
-        print(chr(j), end=" ")
-    print()
--- Day 8/Half pyramid pattern.py
+++
@@ -1,6 +0,0 @@
-# Half Pyramid Pattern
-
-rows = int(input("Enter the number of rows: "))
-
-for i in range(1, rows + 1):
-    print("*" * i)
