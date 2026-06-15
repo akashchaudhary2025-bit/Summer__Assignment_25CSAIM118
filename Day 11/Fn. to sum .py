d69ea8b5d85f0c090dff2b545f986d57bac8d1b9
akashchaudhary2025-bit <akash.chaudhary2025@glbajajgroup.org>
2026-06-15 17:20:00 +0530

    Delete Day 11 directory

--- Day 11/Fn. to sum .py
+++
@@ -1,9 +0,0 @@
-# Function to find sum of two numbers
-
-def add_numbers(a, b):
-    return a + b
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-print("Sum =", add_numbers(num1, num2))
